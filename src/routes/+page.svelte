a282fc05cff23401fbc9185b9222aabeecb24a1e
Dmitriy Onypko <vaporif@gmail.com>
2024-01-03 23:18:37 +0000

    add flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { invoke } from '@tauri-apps/api/tauri'
-
+  import { Alert } from 'flowbite-svelte';
   let name = ''
   let greetMsg = ''
 
@@ -8,9 +8,16 @@
     greetMsg = await invoke('greet', { name })
   }
 </script>
-
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
 <div>
   <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
   <button on:click="{greet}">Greet</button>
   <p>{greetMsg}</p>
 </div>
